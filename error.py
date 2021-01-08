a5add0e7d64c082e3e4ce348113d6939c4dc78aa
JP-Carr <j.p.carr@lancaster.ac.uk>
2021-01-08 13:50:03 +0000

    ensures the input message is a string

--- error.py
+++ error.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 def error(message, sound=True):
+    message=str(message)
     print(message)
     error_time=datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     if sound==True:
