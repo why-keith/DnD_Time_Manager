526678fea52ae637b78bfaf28e8c05f04382e5dd
JP-Carr <j.p.carr@lancaster.ac.uk>
2021-03-02 23:25:38 +0000

    added "end_session_on_close" entry

--- default_pref.py
+++ default_pref.py
@@ -4,4 +4,5 @@ default_pref={"version":"v0.0.0",
           "new_theme": "Default",
           "set_reminder_option": "date",
           "show_tenday": True,
+          "end_session_on_close": False,
       }
